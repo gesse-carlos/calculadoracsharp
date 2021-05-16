6a2630a51bc1be13d2023788b380ac5354aed35b
Gessé Carlos <gesse.carlos@outlook.com>
2021-05-16 16:16:06 -0300

    Coloquei um loop para o usuario poder continuar o programa porem ele precisa de mudancas

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -15,44 +15,50 @@ namespace Calculadora
             Console.WriteLine("Escolha qual operação deseja realizar: \r\n Soma \r\n Subtração \r\n Multiplicação \r\n Divisão \r\n Mostrar todos os resultados \r\n Sair");
             string operacao = Console.ReadLine();
 
-            switch (operacao)
+            for (int i = 0; i < 5; i++)
             {
-                case "Soma":
-                    Console.WriteLine("Digite o primeiro número da operação");
-                    n1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número da operação");
-                    n2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(Soma(n1, n2, resultados));
-                    break;
-                case "Subtração":
-                    Console.WriteLine("Digite o primeiro número da operação");
-                    n1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número da operação");
-                    n2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(Subtracao(n1, n2, resultados));
-                    break;
-                case "Multiplicação":
-                    Console.WriteLine("Digite o primeiro número da operação");
-                    n1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número da operação");
-                    n2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(Multiplicacao(n1, n2, resultados));
-                    break;
-                case "Divisão":
-                    Console.WriteLine("Digite o primeiro número da operação");
-                    n1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo número da operação");
-                    n2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(Divisao(n1, n2, resultados));
-                    break;
-                case "Mostrar todos os resultados":
-                    Console.WriteLine(resultados);
-                    break;
-                default:
-                    Console.WriteLine("Você precisa escolher uma operação");
-                    break;
+                switch (operacao)
+                {
+                    case "Soma":
+                        Console.WriteLine("Digite o primeiro número da operação");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número da operação");
+                        n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine(Soma(n1, n2, resultados));
+                        continue;
+                    case "Subtração":
+                        Console.WriteLine("Digite o primeiro número da operação");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número da operação");
+                        n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine(Subtracao(n1, n2, resultados));
+                        continue;
+                    case "Multiplicação":
+                        Console.WriteLine("Digite o primeiro número da operação");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número da operação");
+                        n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine(Multiplicacao(n1, n2, resultados));
+                        continue;
+                    case "Divisão":
+                        Console.WriteLine("Digite o primeiro número da operação");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o segundo número da operação");
+                        n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine(Divisao(n1, n2, resultados));
+                        continue;
+                    case "Mostrar todos os resultados":
+                        Console.WriteLine(resultados);
+                        continue;
+                    case "Sair":
+                        break;
+                    default:
+                        Console.WriteLine("Você precisa escolher uma operação");
+                        break;
 
+                }
             }
+            
         }
         private static double Soma(double n1, double n2, List<double> resultados)
         {
