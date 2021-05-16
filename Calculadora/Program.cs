c47e863667bdc9be555e86b991e760add20d142b
Gessé Carlos <gesse.carlos@outlook.com>
2021-05-16 12:07:28 -0300

    Defini as primeiras funcoes

---
+++ Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static double Soma(double n1, double n2)
+            {
+                return n1 + n2;
+            }
+            static double Subtracao(double n1, double n2)
+            {
+                return n1 - n2;
+            }
+            static double Multiplicacao(double n1, double n2)
+            {
+                return n1 * n2;
+            }
+            static double Divisao(double n1, double n2)
+            {
+                return n1 / n2;
+            }
+        }
+    }
+}
